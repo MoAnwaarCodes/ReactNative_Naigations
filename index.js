3d688cf017712129a6314b6e023f29f37030c585
Muhammad Anwaar <anwarOfficial9@gmail.com>
2024-09-11 22:50:38 +0500

    Added PushNotification File Seprately

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import App from './App';
 import {name as appName} from './app.json';
 import Card from './Components/UI_Practice/Card';
 import BundleSet from './Components/UI_Practice/Bundle_Set';
-AppRegistry.registerComponent(appName, () => App);
+import PushNotification from './Components/PushNotification/PushNotification';
+AppRegistry.registerComponent(appName, () => PushNotification);
