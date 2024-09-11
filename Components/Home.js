31215bbc613542202d6ccaa78bacbe37a535be7e
Muhammad Anwaar <anwarOfficial9@gmail.com>
2024-09-11 20:44:37 +0500

    Completed the push Notification

---
+++ Components/Home.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={{fontSize:32,color:'black'}}>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
---
+++ Components/Settings.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <View>
+      <Text style={{color:'black',fontSize:32}}>Setting</Text>
+    </View>
+  )
+}
+
+export default Settings
+
+const styles = StyleSheet.create({})
